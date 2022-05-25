847a43671468daeb7330ce019d31c2b67c027f3e
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-05-25 12:23:03 -0300

    Update install-apache-ubuntu.sh

--- install-apache-ubuntu.sh
+++ install-apache-ubuntu.sh
@@ -5,9 +5,9 @@ systemctl start apache2
 systemctl enable apache2
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 
-export AWS_ACCESS_KEY_ID="ASIATRXVMINHAPICAIES"
-export AWS_SECRET_ACCESS_KEY="DesenrolaBateEJogaDeLadinho"
-export AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
+export AWS_ACCESS_KEY_ID="AKIA2NVV7SVMXRBM34ON"
+export AWS_SECRET_ACCESS_KEY="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"
+# export AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
 
 aws_access_key_id=ASIATRXVMINHAPICAIES
 aws_secret_access_key=DesenrolaBateEJogaDeLadinho
@@ -19,3 +19,5 @@ aws_session_token=DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#44334343
 AWS_ACCESS_KEY_ID="ASIATRXVMINHAPICAIES"
 AWS_SECRET_ACCESS_KEY="DesenrolaBateEJogaDeLadinho"
 AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
+
+
