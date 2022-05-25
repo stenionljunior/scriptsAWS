7b3f77a3067471054aea881227a9f52729a392dd
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-05-25 12:13:30 -0300

    Update install-apache-ubuntu.sh

--- install-apache-ubuntu.sh
+++ install-apache-ubuntu.sh
@@ -8,3 +8,7 @@ echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 export AWS_ACCESS_KEY_ID="ASIATRXVMINHAPICAIES"
 export AWS_SECRET_ACCESS_KEY="DesenrolaBateEJogaDeLadinho"
 export AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
+
+aws_access_key_id=ASIATRXVMINHAPICAIES
+aws_secret_access_key=DesenrolaBateEJogaDeLadinho
+aws_session_token=DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434
