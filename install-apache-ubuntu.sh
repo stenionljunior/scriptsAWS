ace3b5f18b7f385d781e77866ff654fc95c0d119
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-05-26 20:43:39 -0300

    Update install-apache-ubuntu.sh

--- install-apache-ubuntu.sh
+++ install-apache-ubuntu.sh
@@ -4,25 +4,3 @@ apt install -y apache2
 systemctl start apache2
 systemctl enable apache2
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-
-export AWS_ACCESS_KEY_ID="AKIA2NVV7SVMXRBM34ON"
-export AWS_SECRET_ACCESS_KEY="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"
-# export AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
-
-aws_access_key_id=ASIATRXVMINHAPICAIES
-aws_secret_access_key=DesenrolaBateEJogaDeLadinho
-aws_session_token=DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434
-
-
-
-
-AWS_ACCESS_KEY_ID="ASIATRXVMINHAPICAIES"
-AWS_SECRET_ACCESS_KEY="DesenrolaBateEJogaDeLadinho"
-AWS_SESSION_TOKEN="DesenrolaBateEJogaDeLadinhoSeuHackudoDosInfernossssss#443343434343434"
-
-
-username=stenio.junior
-password=PegaMinhaSenhabb
-
-usuario=usuario.usuario
-senha=essaehminhasenha
